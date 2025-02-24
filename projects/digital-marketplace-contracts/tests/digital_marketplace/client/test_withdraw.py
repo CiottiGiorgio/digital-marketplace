c140da70611074e14610f631f2c677318d01c317
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2025-02-24 10:36:41 +0000

    chore: added more asserts in the withdraw test

--- projects/digital-marketplace-contracts/tests/digital_marketplace/client/test_withdraw.py
+++ projects/digital-marketplace-contracts/tests/digital_marketplace/client/test_withdraw.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+from algosdk.error import AlgodHTTPError
 import consts as cst
 import pytest
 from algokit_utils import (
@@ -69,12 +70,48 @@ def test_fail_overdraft_withdraw(
 
 def test_pass_withdraw(
     dm_client: DigitalMarketplaceClient,
+    algorand_client: AlgorandClient,
     deposit_into_dm: Callable,
     random_account: SigningAccount,
 ) -> None:
-    result = dm_client.send.withdraw(
-        WithdrawArgs(amount=cst.AMOUNT_TO_DEPOSIT),
+    balance_before_call = algorand_client.account.get_information(
+        random_account.address
+    ).amount
+    deposited_before_call = dm_client.state.local_state(
+        random_account.address
+    ).deposited
+
+    dm_client.send.withdraw(
+        WithdrawArgs(amount=AlgoAmount.from_algo(cst.AMOUNT_TO_DEPOSIT).micro_algo // 2),
         params=CommonAppCallParams(extra_fee=AlgoAmount.from_micro_algo(1_000)),
     )
 
-    assert result.confirmation
+    assert (
+        algorand_client.account.get_information(random_account.address).amount
+        - balance_before_call
+        == AlgoAmount.from_algo(cst.AMOUNT_TO_DEPOSIT).micro_algo // 2
+        - AlgoAmount.from_micro_algo(2_000).micro_algo
+    )
+    assert (
+        dm_client.state.local_state(random_account.address).deposited
+        - deposited_before_call
+        == -AlgoAmount.from_algo(cst.AMOUNT_TO_DEPOSIT).micro_algo // 2
+    )
+
+    balance_before_call = algorand_client.account.get_information(
+        random_account.address
+    ).amount
+
+    dm_client.send.close_out.withdraw(
+        WithdrawArgs(amount=AlgoAmount.from_algo(cst.AMOUNT_TO_DEPOSIT).micro_algo // 2),
+        params=CommonAppCallParams(extra_fee=AlgoAmount.from_micro_algo(1_000)),
+    )
+
+    assert (
+        algorand_client.account.get_information(random_account.address).amount
+        - balance_before_call
+        == AlgoAmount.from_algo(cst.AMOUNT_TO_DEPOSIT).micro_algo // 2
+        - AlgoAmount.from_micro_algo(2_000).micro_algo
+    )
+    with pytest.raises(AlgodHTTPError, match="account application info not found"):
+        _ = dm_client.state.local_state(random_account.address).deposited
